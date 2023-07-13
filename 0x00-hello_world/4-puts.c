abde46f6f5c4722c84e5e50d53233133f7d5ad59
Yahya <yahyaoulha0@gmail.com>
2023-07-13 04:31:16 -0700

    got it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+/**
+ *  * main - Entry point
+ *   * 
+ *    * Description: 'main function will print a text which is \"Programming is like building a multilingual puzzle'
+ *     * 
+ *      * 
+ *       * Return: Always 42 (Success)
+ *        */
 
-int main () {
+int main(void)
+{
 
 	char helloworld[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(helloworld);
 
-	return 0;
+	return (42);
 }
