6af4a2a007dd31101150d47759540dd7ccf01822
Yahya <yahyaoulha0@gmail.com>
2023-07-14 04:59:45 -0700

    first try

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'main function will print the lowercase alphabet'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int lk;
+
+	for (lk = 97; lk <= 122; lk++)
+	{
+		putchar(lk);
+	}
+
+	return (0);
+}
