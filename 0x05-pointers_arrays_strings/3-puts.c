6ccb1c386f81abdedbb37f5c22d547bfcb130468
Yahya <yahyaoulha0@gmail.com>
2023-07-24 05:23:36 -0700

    worked

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - print string and new line.
+ * @str: is a string of characters.
+ * Return no return.
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
