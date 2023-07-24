865416abaa3d22a314325f7bf3f3f9c482b161b0
Yahya <yahyaoulha0@gmail.com>
2023-07-24 04:34:03 -0700

    fixde

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string.
+ * @str: is a string.
+ * Return: returns the length.
+ */
+
+int _strlen(char *str)
+{
+	size_t length = 0;
+
+	while (str[siz] != '\0')
+	{
+		siz++;
+	}
+
+	return (siz);
+}
