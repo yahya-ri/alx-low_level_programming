04cdca6accf7fdc023c8a43c584a09d96139e0b8
Yahya <yahyaoulha0@gmail.com>
2023-08-02 14:14:22 -0700

    fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,13 +10,13 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
 	{
 		return (1);
-	}
-	else if (y < 0)
-	{
-		return (1 / _pow_recursion(x, -y));
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
