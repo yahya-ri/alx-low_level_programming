36b9b90fb85bcf89636bc2b73077eebb1a264aaf
Yahya <yahyaoulha0@gmail.com>
2023-08-02 14:52:31 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,51 @@
 #include "main.h"
 
 /**
- *
- *
+ * _sqrt_r - finde the integer square root of a given number.
+ * @start: the start of the index of the range.
+ * @end: the end of the index of the range.
+ * @n: the input.
+ * Return: return the integer square root.
  */
 
+int _sqrt_r(int n, int start, int end)
+{
+	int mid, square;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	mid = start + (end - start) / 2;
+	square = mid * mid;
+
+	if (square == n)
+	{
+		return (mid);
+	}
+	else if (square < n)
+	{
+		return (_sqrt_r(n, mid + 1, end));
+	}
+	else
+	{
+		return (_sqrt_r(n, start, mid - 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - prevents it from being called with a negative value..
+ * @n: the input.
+ * Return: return square root
+ */
 
 int _sqrt_recursion(int n)
 {
-	int start, end, mid, square;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
 
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-
-	start = 0;
-	end = n;
-
-	while (start <= end)
-	{
-		mid = start + (end - start) / 2;
-		square = mid * mid;
-
-		if (square == n)
-		{
-			return (mid);
-		}
-		else if (square < n)
-		{
-			start = mid + 1;
-		}
-		else
-		{
-			end = mid - 1;
-		}
-	}
-
-	return (-1);
+	return (_sqrt_r(n, 0, n));
 }
