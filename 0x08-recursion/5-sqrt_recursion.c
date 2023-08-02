9c0e1312f070bb2e68e3f205063e428d9304a2d7
Yahya <yahyaoulha0@gmail.com>
2023-08-02 14:57:17 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,37 @@
 #include "main.h"
 
 /**
- * _sqrt_r - finde the integer square root of a given number.
- * @start: the start of the index of the range.
- * @end: the end of the index of the range.
- * @n: the input.
- * Return: return the integer square root.
+ * power_operation - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
  */
 
-int _sqrt_r(int n, int start, int end)
+int power_operation(int n, int c)
 {
-	int mid, square;
-
-	if (start > end)
+	if (c % (n / c) == 0)
 	{
-		return (-1);
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
 	}
-
-	mid = start + (end - start) / 2;
-	square = mid * mid;
-
-	if (square == n)
-	{
-		return (mid);
-	}
-	else if (square < n)
-	{
-		return (_sqrt_r(n, mid + 1, end));
-	}
-	else
-	{
-		return (_sqrt_r(n, start, mid - 1));
-	}
+	return (0 + power_operation(n, c + 1));
 }
 
 /**
- * _sqrt_recursion - prevents it from being called with a negative value..
- * @n: the input.
- * Return: return square root
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-
-	return (_sqrt_r(n, 0, n));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
