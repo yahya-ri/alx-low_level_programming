d30e058bee42d5c9e41c18ff047ba9592f7d2746
Yahya <yahyaoulha0@gmail.com>
2023-08-04 03:25:07 -0700

    worked

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+c=( *.c )
+c_n=${#c[@]}
+counter=0
+
+while [ "$counter" -lt "$c_n" ]; do
+    file="${c[counter]}"
+    o_n=$(basename "$file" .c)
+    gcc -c "$file" -o "${o_n}.o"
+
+    (( counter++ ))
+done
+
+o_f=$(ls *.o 2>/dev/null)
+ar rcs libmy.a $o_f
