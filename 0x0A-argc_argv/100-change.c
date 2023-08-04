27e7ffeba96b1d359d5dcb191e1c62cb3d4090fd
Yahya <yahyaoulha0@gmail.com>
2023-08-04 07:56:20 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int cents, num_coins = 0;
 
-	if (argc == 2 || argc > 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -22,26 +22,16 @@ int main(int argc, char *argv[])
 	while (cents > 0)
 	{
 		if (cents >= 25)
-		{
 			cents -= 25;
-		}
 		else if (cents >= 10)
-		{
 			cents -= 10;
-		}
 		else if (cents >= 5)
-		{
 			cents -= 5;
-		}
 		else if (cents >= 2)
-		{
 			cents -= 2;
-		}
-		else
-		{
+		else if (cents >= 1)
 			cents -= 1;
-		}
-		num_coins++;
+		num_coins += 1;
 	}
 
 	printf("%d\n", num_coins);
