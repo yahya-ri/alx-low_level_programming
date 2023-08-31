1eafc3ae4a4749561d5d3425560f9ccf3c90c82c
Yahya <yahyaoulha0@gmail.com>
2023-08-31 19:19:15 +0100

    worked

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert a binary number to an unsigned int.
+ * @b: binary.
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	len = 0;
+	while (b[len] != '\0')
+	{
+		len++;
+	}
+
+	len--;
+	base_two = 1;
+
+	while (len >= 0) {
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return 0;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+
+		len--;
+		base_two *= 2;
+	}
+
+
+	return (ui);
+}
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
