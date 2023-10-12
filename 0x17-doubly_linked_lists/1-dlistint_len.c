941d4f29ecc5603fafb2243d0f5393406d59a796
Yahya <yahyaoulha0@gmail.com>
2023-10-12 12:56:30 +0100

    worked

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	for (; h->prev != NULL; h = h->prev)
+	{
+	}
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
