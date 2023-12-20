43eb7e3be478ff6c9eddc5c48f3878872920b08d
Yahya <yahyaoulha0@gmail.com>
2023-12-20 12:33:31 +0100

    fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 """the island_perimeter function."""
 
 
 def island_perimeter(grid):
-    """Function that returns the perimeter of island described in grid."""
-
-    result = 0
+	"""Function that returns the perimeter of island described in grid."""
+	result = 0
 	rows = len(grid)
 	if grid != []:
 		cols = len(grid[0])
-	
+
 	for i in range(rows):
 		for j in range(cols):
 			if grid[i][j] == 1:
@@ -19,6 +17,6 @@ def island_perimeter(grid):
 					result -= 2
 				if j > 0 and grid[i][j - 1] == 1:
 					result -= 2
-	
+
 	return result
 
