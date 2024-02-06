17ea51f04c5858acea089e2199e0d6d32073f623
Yahya <yahyaoulha0@gmail.com>
2024-02-06 17:53:08 +0100

    ya bsmlah

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,19 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	for (i = 0; i <= size; i++)
 	{
-		printf("Value checked array[%lu] = [%i]\n", (unsigned long)i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
 
 		if (value == array[i])
 		{
 			return (i);
 		}
 	}
-	return (1);
+	return (-1);
 }
